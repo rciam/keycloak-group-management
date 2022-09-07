6daaa3e97dbbbc405984d17883c979507e702899
laskasn <laskarisn@admin.grnet.gr>
2022-09-07 14:46:07 +0300

    account UI sceleton

--- src/main/resources/theme/groups/account/src/app/groups-mngnt-service/groups.service.ts
+++ src/main/resources/theme/groups/account/src/app/groups-mngnt-service/groups.service.ts
@@ -1,24 +1,13 @@
-/*
- * Copyright 2018 Red Hat Inc. and/or its affiliates and other contributors
- * as indicated by the @author tags. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
+
 
 // @ts-ignore
-import {KeycloakService} from '../keycloak-service/keycloak.service';
+import { KeycloakClient, KeycloakService } from '../keycloak-service/keycloak.service';
 // @ts-ignore
-import {ContentAlert} from '../content/ContentAlert';
+import { ContentAlert } from '../content/ContentAlert';
+
+
+declare const keycloak: KeycloakClient;
+const keycloakService = new KeycloakService(keycloak);
 
 declare const baseUrl: string;
 
@@ -43,9 +32,9 @@ export class GroupsServiceClient {
     private kcSvc: KeycloakService;
     private groupsUrl: string;
 
-    public constructor(keycloakService: KeycloakService) {
+    public constructor() {
         this.kcSvc = keycloakService;
-        this.groupsUrl = this.kcSvc.authServerUrl() + 'realms/' + this.kcSvc.realm() + '/groups';
+        this.groupsUrl = this.kcSvc.authServerUrl() + 'realms/' + this.kcSvc.realm() + '/groups-management';
     }
 
     public async doGet<T>(endpoint: string,
