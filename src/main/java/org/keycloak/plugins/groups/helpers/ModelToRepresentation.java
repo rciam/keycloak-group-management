74a50098afa101405b643aab59caae2a089eb497
Konstantinos Georgilakis <cgeorgilakis@grnet.gr>
2023-03-20 16:29:53 +0200

    Add group path to admin groups pager

--- src/main/java/org/keycloak/plugins/groups/helpers/ModelToRepresentation.java
+++ src/main/java/org/keycloak/plugins/groups/helpers/ModelToRepresentation.java
@@ -18,38 +18,22 @@ public class ModelToRepresentation extends org.keycloak.models.utils.ModelToRepr
      * @param full
      * @return
      */
-    public static GroupRepresentation toRepresentation(GroupModel group, boolean full, boolean hasRights) {
+    public static GroupRepresentation toRepresentation(GroupModel group, boolean full) {
         GroupRepresentation rep = new GroupRepresentation();
         rep.setId(group.getId());
         rep.setName(group.getName());
-        rep.setHasRights(hasRights);
         if ( full)
            rep.setPath(buildGroupPath(group));
         return rep;
     }
 
-    public static GroupRepresentation toGroupHierarchy(GroupModel group, boolean full) {
-        GroupRepresentation rep = toSimpleGroupHierarchy(group, full);
-        if ( group.getParent() != null ) {
-            GroupModel parentGroup = group.getParent();
-            do {
-                GroupRepresentation repChild = rep;
-                rep = toRepresentation(parentGroup, full, false);
-                rep.setExtraSubGroups(Stream.of(repChild).collect(Collectors.toList()));
-                parentGroup = parentGroup.getParent();
-            } while(parentGroup != null);
-        }
-        return rep;
-    }
-
     public static GroupRepresentation toSimpleGroupHierarchy(GroupModel group, boolean full) {
-        GroupRepresentation rep = toRepresentation(group, full, true);
+        GroupRepresentation rep = toRepresentation(group, full);
         List<GroupRepresentation> subGroups = group.getSubGroupsStream()
                 .map(subGroup -> toSimpleGroupHierarchy(subGroup, full)).collect(Collectors.toList());
         rep.setExtraSubGroups(subGroups);
         return rep;
     }
-
 
 
 }
